d5bdcf51bfc91ee47d03c7d5fb253998db022e3f
heejae <13478qw@naver.com>
2024-03-26 16:39:05 +0900

    Finish

---
+++ src/Ex501.java
@@ -0,0 +1,19 @@
+
+public class Ex501 {
+
+		public static void hello(String name) {
+			System.out.println("Hello, " + name);
+			
+			
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		hello("Aaron");
+		hello("Tom");
+		hello("Loki");
+		hello("Rocky");
+		hello("Socky");
+		hello("");
+	}
+
+}
